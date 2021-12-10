a159619b1d8a120b9bb6000c4f0cff147747ff6f
pao-f <pmlforte@gmail.com>
2021-12-10 23:17:59 +0100

    New files for database managing

--- controllers/cpu.route.js
+++ controllers/cpu.route.js
@@ -6,9 +6,66 @@ const brandRepo = require('../utils/brands.repository');
 const cpuRepo = require('../utils/cpu.repository');
 const gpuRepo = require('../utils/gpu.repository');
 
-router.get('/', computerRootAction);
-router.get('/list', computerListAction);
-router.get('/show/:computerId', computerShowAction);
-router.get('/del/:computerId', computerDelAction);
-router.get('/edit/:computerId', computerEditAction);
-router.post('/update/:computerId', computerUpdateAction);
+router.get('/', cpuRootAction);
+router.get('/list', cpuListAction);
+router.get('/show/:cpuId', cpuShowAction);
+router.get('/del/:cpuId', cpuDelAction);
+router.get('/edit/:cpuId', cpuEditAction);
+router.post('/update/:cpuId', cpuUpdateAction);
+
+// http://localhost:9000/computers
+function cpuRootAction(request, response) {
+    //response.send("ROOT ACTION");
+    response.redirect("/cpu/list");
+}
+async function cpuListAction(request, response) {
+    // response.send("LIST ACTION");
+    var cpus = await cpuRepo.getAllCPU();
+    var flashMessage = request.session.flashMessage;
+    request.session.flashMessage = "";
+
+    response.render("cpus_list", { "cpus": cpus, "flashMessage": flashMessage });
+}
+async function cpuShowAction(request, response) {
+    // response.send("SHOW ACTION");
+    var oneCpu = await cpuRepo.getOneCpu(request.params.cpuId);
+    response.render("cpus_show", { "oneCpu": oneCpu });
+}
+async function cpuEditAction(request, response) {
+    // response.send("EDIT ACTION");
+    var brands = await brandRepo.getAllBrands();
+    var cpus = await cpuRepo.getAllCPU();
+
+    var cpuId = request.params.cpuId;
+    console.log(cpu);
+    if (cpuId!=="0")
+        var cpu = await cpuRepo.getOneCpu(cpuId);
+    else
+        var cpu = cpuRepo.getBlankCpu();
+
+    response.render("cpus_edit", { "oneComputer": computer, "oneCpu": cpu, "brands": brands, "cpu": cpus, "gpu": gpu  });
+}
+async function cpuDelAction(request, response) {
+    // response.send("DEL ACTION");
+    // TODO: remove extras for cou, unless the cpu cannot be removed!!!
+    var numRows = await cpuRepo.delOneCpu(request.params.cpuId);
+    request.session.flashMessage = "ROWS DELETED: "+numRows;
+    response.redirect("/cpus/list");
+}
+async function cpuUpdateAction(request, response) {
+    // response.send("UPDATE ACTION");
+    var cpuId = request.params.cpuId;
+    if (cpuId==="0") cpuId = await cpuRepo.addOneCpu(request.body.cpu_brand);
+
+    var numRows = await cpuRepo.editOneCpu(cpuId,
+        request.body.cpu_brand,
+        request.body.cpu_model,
+        request.body.cpu_basefrequency,
+        request.body.cpu_boostfrequency,
+        request.body.cpu_cores,);
+
+    request.session.flashMessage = "ROWS UPDATED: "+numRows;
+    response.redirect("/cpus/list");
+}
+
+module.exports = router;
--- utils/gpu.repository.js
+++ utils/gpu.repository.js
@@ -1,7 +1,5 @@
 pool = require("../utils/db.js");
 
-
-
 module.exports = {
     getBlankGpu(){ // defines the entity model
         return {
@@ -16,7 +14,7 @@ module.exports = {
     async getAllGPU(){
         try {
             conn = await pool.getConnection();
-            sql = "SELECT * FROM gpu";
+            sql = "SELECT * FROM gpu INNER JOIN brands ON gpu_brand=brand_id";
             const rows = await conn.query(sql);
             conn.end();
             return rows;
@@ -26,4 +24,65 @@ module.exports = {
             throw err; // return false ???
         }
     },
-}
+
+    async getOneGpu(gpuId){
+        try {
+            conn = await pool.getConnection();
+            // sql = "SELECT * FROM computers INNER JOIN brands ON computer_brand=brand_id WHERE computer_id = "+computerId; // SQL INJECTION => !!!!ALWAYS!!!! sanitize user input!
+            // escape input OR prepared statements OR use orm
+            sql = "SELECT * FROM gpu INNER JOIN brands ON gpu_brand=brand_id WHERE gpu_id = ?";
+            const rows = await conn.query(sql, gpuId);
+            conn.end();
+            console.log("ROWS FETCHED: "+rows.length);
+            if (rows.length == 1) {
+                return rows[0];
+            } else {
+                return false;
+            }
+        }
+        catch (err) {
+            throw err;
+        }
+    },
+
+    async delOneGpu(gpuId){
+        try {
+            conn = await pool.getConnection();
+            sql = "DELETE FROM gpu WHERE gpuId = ?";
+            const okPacket = await conn.query(sql, gpuId); // affectedRows, insertId
+            conn.end();
+            console.log(okPacket);
+            return okPacket.affectedRows;
+        }
+        catch (err) {
+            throw err;
+        }
+    },
+    async addOneGpu(brandId){ // ATTENTION QUEL PARA ???
+        try {
+            conn = await pool.getConnection();
+            sql = "INSERT INTO computers (gpu_id, computer_brand) VALUES (NULL, ?) ";
+            const okPacket = await conn.query(sql, brandId); // affectedRows, insertId
+            conn.end();
+            console.log(okPacket);
+            return okPacket.insertId;
+        }
+        catch (err) {
+            throw err;
+        }
+    },
+    async editOneGpu(gpuId, gpuBrand, gpuModel, gpuFillratePixel, gpuCu, gpuMemory){
+        try {
+            conn = await pool.getConnection();
+            sql = "UPDATE gpu SET gpuBrand=?, gpuModel=?, gpuFillratePixel=?, gpuCu=?, gpuMemory=?, WHERE gpu_id=? "; // TODO: named parameters? :something
+            const okPacket = await conn.query(sql,
+                [gpuBrand, gpuModel, gpuFillratePixel, gpuCu, gpuMemory]);
+            conn.end();
+            console.log(okPacket);
+            return okPacket.affectedRows;
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+};
