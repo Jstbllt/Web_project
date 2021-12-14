f2bfa522a85269a31aa2728d213acc5439b5a380
Jstbllt <justine.boillot@efrei.net>
2021-12-14 15:58:23 +0100

    Final Version

--- utils/db.js
+++ utils/db.js
@@ -6,8 +6,7 @@ const pool = mariadb.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+    database: process.env.DB_DATABASE
 });
 
 module.exports = {
